924802e027862a53b8605037529a8263520f751c
majd eddine oumanni <majdeddineoumanni@gmail.com>
2024-12-16 09:03:00 +0100

    updating functions and adding three functions

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,54 @@
 import java.util.Scanner;
 
+
 public class Main {
-    public static void add(double num1, double num2){
+    static double num1, num2;
+    static double number;
+    public static void twoNumbers(){
+        Scanner num = new Scanner(System.in);
+
+        System.out.print("entre premiere nombre: ");
+        num1 = num.nextDouble();
+
+        System.out.print("entre deuxiem nombre: ");
+        num2 = num.nextDouble();
+    }
+    public static void oneNumber(){
+        Scanner num = new Scanner(System.in);
+        System.out.print("entre une nombre: ");
+        number = num.nextDouble();
+    }
+    public static void menu(){
+        System.out.println("-----Menu-----");
+        System.out.println("  1. pour L'addition");
+        System.out.println("  2. pour la Soustraction");
+        System.out.println("  3. pour la Multiplication");
+        System.out.println("  4. pour la Division");
+        System.out.println("  5. pour calculet premier nombre à la puissance du deuxième nombre");
+        System.out.println("  6. pour calculet le Racine carrée");
+        System.out.println("  7. pour la factorielle");
+        System.out.println("  0. pour quitter");
+    }
+    public static void add(){
+        twoNumbers();
         double total = num1 + num2;
         String result = String.format("\n  %.2f + %.2f = %.2f  \n", num1, num2, total);
         System.out.println(result);
     }
-    public static void sub(double num1, double num2){
+    public static void sub(){
+        twoNumbers();
         double subNumber = num1 - num2;
         String result = String.format("\n  %.2f - %.2f = %.2f  \n", num1, num2, subNumber);
         System.out.println(result);
     }
-    public static void mult(double num1, double num2){
+    public static void mult(){
+        twoNumbers();
         double multNumber = num1 * num2;
         String result = String.format("\n  %.2f x %.2f = %.2f  \n", num1, num2, multNumber);
         System.out.println(result);
     }
-    public static void div(double num1, double num2){
+    public static void div(){
+        twoNumbers();
         double divNumber = num1 / num2;
         if(num2 == 0){
             System.out.println("Error");
@@ -25,92 +57,61 @@ public class Main {
             System.out.println(result);
         }
     }
-    public static void power(double num1, double num2){
+    public static void power(){
+        twoNumbers();
         double powerNumber = Math.pow(num1, num2);
         String result = String.format("\n  %.2f ^ %.2f = %.2f  \n", num1, num2, powerNumber);
         System.out.println(result);
     }
-    public static void squareRoot(double num1, double num2){
-        Scanner choiceNum = new Scanner(System.in);
-        System.out.print("écrivez 1 pour le premier nombre ou 2 pour le deuxième nombre: ");
-        int number = choiceNum.nextInt();
+    public static void squareRoot(){
+        oneNumber();
         double sqrt;
-        if (number == 1){
-            sqrt = Math.sqrt(num1);
-            String result1 = String.format("√%.2f = %.2f", num1, sqrt);
-            System.out.println(result1);
-        } else if (number == 2) {
-            sqrt = Math.sqrt(num2);
-            String result2 = String.format("√%.2f = %.2f", num2, sqrt);
-            System.out.println(result2);
-        }else{
-            System.out.println("non valid");
-        }
+        sqrt = Math.sqrt(number);
+        String result = String.format(" \n √%.2f = %.2f  \n ", number, sqrt);
+        System.out.println(result);
     }
 
-    public static void factorial(double num1, double num2) {
-        Scanner choice = new Scanner(System.in);
-        System.out.print("Écrivez 1 pour le premier nombre ou 2 pour le deuxième nombre: ");
-        double choiceNumber = choice.nextDouble();
+
+    public static void factorial() {
+        oneNumber();
         double factorial = 1;
-
-        if (choiceNumber == 1) {
-            for (int i = 1; i <= num1; i++) {
-                factorial *= i;
-            }
-            String result1 = String.format("Le factorial de %.2f est %.2f", num1, factorial);
-            System.out.println(result1);
-        } else if (choiceNumber == 2) {
-            for (int i = 1; i <= num2; i++) {
-                factorial *= i;
-            }
-            String result2 = String.format("Le factorial de %.2f est %.2f", num2, factorial);
-            System.out.println(result2);
-        } else {
-            System.out.println("Choix non valide");
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
         }
+        String result = String.format(" \n Le factorial de %.2f est %.2f \n ", number, factorial);
+        System.out.println(result);
     }
+
 
     public static void main(String[] args){
 
+
         while (true){
-            Scanner num = new Scanner(System.in);
-            System.out.print("entre nombre 1: ");
-            double num1 = num.nextDouble();
-            System.out.print("entre nombre 2: ");
-            double num2 = num.nextDouble();
+            menu();
             Scanner chnum = new Scanner(System.in);
-            System.out.println("  1. pour L'addition");
-            System.out.println("  2. pour la Soustraction");
-            System.out.println("  3. pour la Multiplication");
-            System.out.println("  4. pour la Division");
-            System.out.println("  5. pour calculet premier nombre à la puissance du deuxième nombre");
-            System.out.println("  6. pour calculet le Racine carrée");
-            System.out.println("  7. pour la factorielle");
-            System.out.println("  0. pour quitter");
             System.out.print("entre votre choix: ");
             int choice = chnum.nextInt();
             switch (choice){
                 case 1:
-                    add(num1, num2);
+                    add();
                     break;
                 case 2:
-                    sub(num1, num2);
+                    sub();
                     break;
                 case 3:
-                    mult(num1, num2);
+                    mult();
                     break;
                 case 4:
-                    div(num1, num2);
+                    div();
                     break;
                 case 5:
-                    power(num1, num2);
+                    power();
                     break;
                 case 6:
-                    squareRoot(num1, num2);
+                    squareRoot();
                     break;
                 case 7:
-                    factorial(num1, num2);
+                    factorial();
                     break;
                 case 0:
                     System.out.println("\n quitter \n");
@@ -121,3 +122,4 @@ public class Main {
         }
     }
 }
+
